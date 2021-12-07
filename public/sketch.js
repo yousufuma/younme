cdd1526d094f5e8de1ca4fdca4bd32c8ac2bb903
yousufuma <51690864+yousufuma@users.noreply.github.com>
2021-12-07 12:35:41 +0800

    Initial commit

---
+++ public/sketch.js
@@ -0,0 +1,113 @@
+let obj, obj2, pg;
+
+let myvideo;
+
+let otherVideo = {};
+
+let p5l;
+
+let socket;
+
+function preload() {
+  obj = loadModel("neo.obj", true);
+  obj2 = loadModel("x.obj", true);
+  // sound = loadSound('002.mp3');
+}
+
+function setup() {
+  createCanvas(622, 622, WEBGL);
+  pg = createGraphics(width, height);
+  angleMode(DEGREES);
+  textureMode(IMAGE);
+  textureWrap(CLAMP);
+
+  socket = io.connect('http://localhost:3000')
+
+  let constraints = { audio: true, video: true };
+  myvideo = createCapture(constraints, function (stream) {
+    p5l = new p5LiveMedia(this, "CAPTURE", stream, "Shared Space");
+    p5l.on("stream", gotStream);
+    p5l.on("disconnect", gotDisconnect);
+  });
+  myvideo.elt.muted = true;
+  myvideo.hide();
+}
+
+function draw() {
+  // clear();
+
+  stroke(200);
+
+  rotateZ(180);
+
+  rotateY(58);
+
+  pg.clear();
+  pg.push();
+  // pg.background("white");
+  pg.translate(pg.width / 2, pg.height);
+  pg.rotate(radians(180));
+  pg.textAlign(LEFT, CENTER);
+
+  pg.randomSeed(4);
+  for (let i = 0; i < 44; i++) {
+    let txtSize = 122;
+    let x =
+      ((pg.random(pg.width) + frameCount) % (pg.width + txtSize)) -
+      pg.width / 5;
+    let y = i * 40;
+    pg.fill("green");
+    pg.stroke("green");
+    pg.textSize(65);
+    pg.text("Me和你&我和You", -x, y);
+  }
+  pg.pop();
+  textureMode(IMAGE);
+  textureWrap(CLAMP);
+
+  push();
+  translate(486, 10, -300);
+  scale(2.75);
+  rotateY(frameCount * 0.3);
+  strokeWeight(0);
+  noStroke();
+  //   ambientLight(180);
+  //   let locX = mouseX - width / 2;
+  //   let locY = mouseY - height / 2;
+  //   pointLight(100, 0, 0, locX, locY, 50);
+
+  //   specularMaterial(200,200);
+  //   shininess(20);
+  texture(pg);
+  sphere(185);
+
+  pop();
+
+  scale(2.45);
+  rotateY(-frameCount * 2);
+  noStroke();
+  texture(myvideo);
+  model(obj);
+
+  let count = 1;
+  for (const id in otherVideo) {
+    scale(1);
+    rotateY(-frameCount * 1);
+    noStroke();
+
+    texture(otherVideo[id].stream);
+    model(obj2);
+
+    count++;
+  }
+}
+
+// We got a new stream!
+function gotStream(stream, id) {
+  stream.hide();
+  otherVideo[id] = { stream: stream };
+}
+
+function gotDisconnect(id) {
+  delete otherVideo[id];
+}
---
+++ server.js
@@ -0,0 +1,147 @@
+// let fs = require('fs');
+let express = require('express');
+let app = express();
+
+// var cors = require('cors');
+// app.use(cors(
+// {
+// 	origin: /p5js\.org$/,
+// 	optionsSuccessStatus: 200 
+// }
+// ));
+
+// let server = app.listen(3000);
+app.use(express.static('public'));
+
+let http = require('http');
+
+let httpServer = http.createServer(app);
+
+let port = process.env.port || 3000;
+
+httpServer.listen(port, ()=>{
+    console.log('Server listening on port ', port);
+  });
+  
+
+let peers = {};
+
+let io = require('socket.io');
+io = new io.Server(httpServer);
+
+io.sockets.on('connection', (socket) => {
+    console.log('We have a new client: ', socket.id);
+    /*STEP 3.1. Add socket to 'peers' object*/
+    peers[socket.id] = socket;
+
+    socket.on('list', () => {
+        //get an ids array
+        let ids = Object.keys(peers);
+        console.log(ids);
+
+        socket.emit('listresults', ids);
+  });
+
+  socket.on('signal', (to, from, data) => {
+    console.log('signal', to, data);
+    if(to in peers){
+        //send signal to that peer
+        peers[to].emit('signal', to, from, data);
+      }else{
+        console.log('Peer not found');
+      };
+    });
+  
+    socket.on('disconnect', () => {
+      console.log('Socket disconnected: ', socket.id);
+  
+      io.emit('peer_disconnect', socket.id);
+  
+      /*STEP 3.2. Delete from 'peers' object*/
+      delete peers[socket.id];
+    });
+  });
+
+    
+
+    
+
+
+
+
+// io.sockets.on('connection', 
+// function (socket) {
+
+//     console.log(Date.now(), socket.id, "New client");
+//     socket.on('room_connect', function(room) {
+//         console.log(Date.now(), socket.id, room, 'room_connect');
+
+//         if (!rooms.hasOwnProperty(room)) {
+//             console.log(Date.now(), socket.id, "room doesn't exist, creating it");
+//             rooms[room] = [];
+//         }
+//         rooms[room].push(socket);
+//         socket.room = room;
+
+//         console.log(Date.now(), socket.id, rooms);
+
+//         let ids = [];
+//         for (let i = 0; i < rooms[socket.room].length; i++) {
+//             ids.push(rooms[socket.room][i].id);
+//         }
+//         console.log(Date.now(), socket.id, "ids length: " + ids.length);
+//         socket.emit('listresults', ids);
+//     });
+
+//     socket.on('list', function() {
+//         let ids = [];
+//         for (let i = 0; i < rooms[socket.room].length; i++) {
+//             ids.push(rooms[socket.room][i].id);
+//         }
+//         console.log(Date.now(), socket.id, "ids length: " + ids.length);
+//         socket.emit('listresults', ids);			
+//     });
+    
+//     // Relay signals back and forth
+//     socket.on('signal', (to, from, data) => {
+//         //console.log("SIGNAL", to, data);
+//         let found = false;
+//         for (let i = 0; i < rooms[socket.room].length; i++) {
+//             //console.log(rooms[socket.room][i].id, to);
+//             if (rooms[socket.room][i].id == to) {
+//                 //console.log("Found Peer, sending signal");
+//                 rooms[socket.room][i].emit('signal', to, from, data);
+//                 found = true;
+//                 break;
+//             }				
+//         }	
+//         // if (!found) {
+//         // 	console.log("never found peer");
+//         // }
+//     });
+            
+//     socket.on('disconnect', function() {
+//         console.log(Date.now(), socket.id, "Client has disconnected");
+//         if (rooms[socket.room]) { // Check on this
+//             // Tell everyone first
+//             let which = -1;
+//             for (let i = 0; i < rooms[socket.room].length; i++) {
+//                 if (rooms[socket.room][i].id != socket.id) {
+//                     rooms[socket.room][i].emit('peer_disconnect', socket.id);
+//                 } else {
+//                     which = i;
+//                 }
+//             }		
+//             // Now remove from array
+//             if (rooms[socket.room][which].id == socket.id) {
+//                 rooms[socket.room].splice(which,1);
+//             }		
+
+//             // This could fail if someone joins while the loops are in progress
+//             // Should be using associative arrays all the way around here
+//         }
+//     });
+// }
+// );
+
+
